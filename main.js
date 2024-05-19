a419b253815fdf0fdc54d2320cca545bc8c86027
iamyyasin <rainman1651@gmail.com>
2024-05-19 22:29:20 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const resBtn = document.getElementById("res-btn");
+const resMenu = document.getElementById("res-menu");
+
+resBtn.addEventListener("click", () => {
+  resMenu.classList.toggle("active");
+});
